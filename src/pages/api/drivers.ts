dc244cf342e0f49df5191946204009fe928aa896
SergioGundin <sergio.gundin.rodriguez@gmail.com>
2024-06-13 22:35:21 +0200

    wip: ready to start consume new API

--- src/pages/api/drivers.ts
+++ src/pages/api/drivers.ts
@@ -1,19 +1,6 @@
-import { getRequestOptions } from '../../db/setup';
-
 export async function GET() {
-  const response = await fetch(`https://v1.formula-1.api-sports.io/drivers`, getRequestOptions());
-
-  if (!response.ok) {
-    return new Response(null, {
-      status: 404,
-      statusText: 'Not found'
-    });
-  }
-
-  const data = await response.json();
-
   return new Response(
-    JSON.stringify(data?.response), {
+    JSON.stringify('drivers'), {
     status: 200,
     headers: {
       "Content-Type": "application/json"
