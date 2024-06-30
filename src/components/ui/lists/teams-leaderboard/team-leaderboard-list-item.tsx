e0e0cdf89b73b9bd499b630f1459f78311d19471
SergioGundin <sergio.gundin.rodriguez@gmail.com>
2024-06-30 20:52:48 +0200

    min changes in team item list

--- src/components/ui/lists/teams-leaderboard/team-leaderboard-list-item.tsx
+++ src/components/ui/lists/teams-leaderboard/team-leaderboard-list-item.tsx
@@ -1,11 +1,23 @@
 import { Card, CardBody, Image } from '@nextui-org/react';
 import type { TeamInterface } from '../../../../api/interfaces/team.interface';
+import { defaultTeamColor, teamColors, type TeamsColorsInterface } from '../../../../lib/team-colors';
+import '../../teams/team.css';
 
 export default function TeamLeaderBoardListItem({ team }: { team: TeamInterface }) {
+  const teamColor = teamColors.find(
+    (teamColor: TeamsColorsInterface) => teamColor.team === team.name
+  ) ?? defaultTeamColor;
+
   return (
     <Card className="flex flex-row justify-between items-center gap-2 w-[100%]">
+      <span
+        className='team-gradient absolute'
+        style={{
+          background: teamColor.gradient,
+        }}
+      />
       <CardBody className='flex flex-row justify-between items-center gap-1'>
-        <div className='flex flex-row justify-start items-center gap-2'>
+        <div className='flex-1 flex flex-row justify-start items-center gap-2'>
           <Image
             src={team.icon ?? ''}
             radius="sm"
@@ -20,7 +32,7 @@ export default function TeamLeaderBoardListItem({ team }: { team: TeamInterface 
             <h4 className="text-small font-semibold leading-none text-default-600">{team.name}</h4>
           </div>
         </div>
-        <div className='flex flex-col items-end justify-center gap-1'>
+        <div className=' flex flex-col items-end justify-center gap-1 w-[20%]'>
           <p className="text-tiny uppercase font-bold">{team.position} POS</p>
           <small className="text-default-500">{team.points} PTS</small>
         </div>
